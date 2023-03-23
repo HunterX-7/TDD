33bd30b858c809477115aff9e5e0e896544b7bbd
HunterX-7 <fraquin7@gmail.com>
2023-03-23 17:30:09 -0500

    Add tests for the factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,14 @@
 require_relative '../classes/solver'
 
 describe Solver do
-    
+    describe '#factorial' do
+        context 'takes one argument, an integer N' do
+            it 'returns the factorial' do
+                factorial_zero = Solver.new(0)
+                factorial_five = Solver.new(5)
+                expect(factorial_zero.factorial).to eq(1)
+                expect(factorial_five.factorial).to eq(120)
+            end
+        end
+    end
 end
