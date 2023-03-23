18721ae5f92a06e9bfd0e2a1cd24282900cc075e
HunterX-7 <fraquin7@gmail.com>
2023-03-23 17:46:53 -0500

    Define the initialize variable in Solver class and create the factorial method

--- classes/solver.rb
+++ classes/solver.rb
@@ -1,3 +1,17 @@
 class Solver
+    def initialize(number)
+        @number = number
+    end
     
+    def factorial
+        if @number == 0
+            return 1
+        else
+            factorial = 1
+            (1..@number).each do |x|
+                factorial = factorial * x
+            end
+            return factorial
+        end
+    end
 end
