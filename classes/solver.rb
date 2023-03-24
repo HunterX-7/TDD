9d0e69f8c4407ccdc35f87251b0c5a491852107e
HunterX-7 <fraquin7@gmail.com>
2023-03-23 19:48:56 -0500

    Create the fizzbuzz method

--- classes/solver.rb
+++ classes/solver.rb
@@ -18,6 +18,18 @@ class Solver
     def reverse
         reverse_string = ''
         @arg.each_char { |x| reverse_string = x + reverse_string }
-        reverse_string
+        return reverse_string
+    end
+
+    def fizzbuzz
+        if @arg % 3 == 0 && @arg % 5 == 0
+            return "fizzbuzz"
+          elsif @arg % 3 == 0
+            return "fizz"
+          elsif @arg % 5 == 0
+            return "buzz"
+          else
+            return @arg.to_s
+        end
     end
 end
