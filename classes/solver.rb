b8e64e4dac8d8968a0b2c0f0d07799b95868448e
HunterX-7 <fraquin7@gmail.com>
2023-03-23 19:49:42 -0500

    Fix linters

--- classes/solver.rb
+++ classes/solver.rb
@@ -1,35 +1,34 @@
 class Solver
-    def initialize(arg)
-        @arg = arg
-    end
-    
-    def factorial
-        if @arg == 0
-            return 1
-        else
-            factorial = 1
-            (1..@arg).each do |x|
-                factorial = factorial * x
-            end
-            return factorial
-        end
-    end
+  def initialize(arg)
+    @arg = arg
+  end
 
-    def reverse
-        reverse_string = ''
-        @arg.each_char { |x| reverse_string = x + reverse_string }
-        return reverse_string
-    end
+  def factorial
+    return 1 if @arg.zero?
 
-    def fizzbuzz
-        if @arg % 3 == 0 && @arg % 5 == 0
-            return "fizzbuzz"
-          elsif @arg % 3 == 0
-            return "fizz"
-          elsif @arg % 5 == 0
-            return "buzz"
-          else
-            return @arg.to_s
-        end
+
+    factorial = 1
+    (1..@arg).each do |x|
+      factorial *= x
     end
+    factorial
+  end
+
+  def reverse
+    reverse_string = ''
+    @arg.each_char { |x| reverse_string = x + reverse_string }
+    reverse_string
+  end
+
+  def fizzbuzz
+    if (@arg % 3).zero? && (@arg % 5).zero?
+      'fizzbuzz'
+    elsif (@arg % 3).zero?
+      'fizz'
+    elsif (@arg % 5).zero?
+      'buzz'
+    else
+      @arg.to_s
+    end
+  end
 end
